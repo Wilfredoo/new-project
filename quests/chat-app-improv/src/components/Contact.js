2487d1302e3bf82da1c394bc8bdf5809a0c517d8
SebasBar <toolt2y@gmail.com>
2020-10-18 11:41:32 +0200

    react 05 completed

--- quests/chat-app-improv/src/components/Contact.js
+++ quests/chat-app-improv/src/components/Contact.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import './Contact.css'
 
-export default function Contact({name, avatar, online}) {
+class Contact extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            online: props.online,
+        }
+    };
+
+    render(){
     return (
         <div className="contact" >
-            <img className="avatar" src={avatar} alt={name} />
+            <img className="avatar" src={this.props.avatar} alt={this.props.name} 
+            />
             <div>
-                <p className="name">{name}</p>
-                <div className="status" >
-                    <div className={online ? 'status-online' : 'status-offline'} />
-                    <p className="status-text">{online ? 'online' : 'offline' }</p>
+                <p className="name">{this.props.name}</p>
+                <div className="status"
+                            onClick = {event => {
+                                const newOnlineStat = !this.state.online;
+                                this.setState({online: newOnlineStat});
+                                 }}>
+                    <div className={this.state.online ? 'status-online' : 'status-offline'}/>
+                    <p className="status-text">{this.state.online ? 'online' : 'offline' }</p>
                 </div>
             </div>
         </div>
     )
 }
+}
+
+export default Contact;
